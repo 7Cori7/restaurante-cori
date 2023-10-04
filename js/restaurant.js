28fcc7649bbf6f877cf0901d0ca7171fc34c2591
7Cori7 <corinaidldesign@gmail.com>
2023-10-04 15:48:13 -0500

    actualizado

--- js/restaurant.js
+++ js/restaurant.js
@@ -106,6 +106,7 @@ function mostrarMenu(menu){
         inputCantidad.value = 0;
         inputCantidad.id = `producto-${pos.id}`;
         inputCantidad.classList.add('col-md-1');
+
         inputCantidad.onchange = function(){ //<--detecta un cambio
             const cantidad = parseInt(inputCantidad.value);
             //console.log({...pos,cantidad})
@@ -124,4 +125,124 @@ function mostrarMenu(menu){
 
         contenido.appendChild(fila);
     })
+}
+
+function agregarOrden(producto){
+
+
+    let {pedido} = cliente; //<---extraigo el objeto pedido del arreglo cliente
+    //console.log(producto)//?<---corroboramos que si hay un cambio en el input lo guarde en el objeto
+
+    const {pro} = cliente.pedido;
+
+    if(producto.cantidad > 0){
+
+        //validar que el producto exista:
+        const existe = pedido.some(item=>item.id === producto.id);
+
+        if(existe){
+            //caso en que haya producto
+            const pedidoActualizado = pedido.map(i=>{
+                if(i.id === producto.id){
+                    i.cantidad = producto.cantidad;
+                }
+                return i;
+            })
+
+            cliente.pedido = [...pedidoActualizado]; //<--se actualiza el arreglo con lo que trae el nuevo arreglo pedidoActualizado
+
+        }else{
+            //caso de que no exista el producto
+            //agregamos el nuevo producto al arreglo
+            cliente.pedido = {...pedido, producto};
+            
+        }
+    }else{
+        //caso en que cantidad es 0
+        const resultado = pedido.filter(item=>item.id !== producto.id);
+        cliente.producto = resultado;
+    }
+
+    limpiarHTML();
+
+    console.log(cliente)
+    
+
+   //if(cliente.pedido.length > 0){
+        actualizarResumen();
+   // }else{
+        //console.log('pedido vacio')
+    //}
+}
+
+function actualizarResumen(){
+    const contenido = document.querySelector('#resumen .contenido');
+    const resumen = document.createElement('div');
+
+    //mostrar la mesa
+    const mesa = document.createElement('p');
+    mesa.textContent = 'Mesa: ';
+    mesa.classList.add('fw-bold');
+
+    const mesaCliente = document.createElement('span');
+    mesaCliente.textContent = cliente.mesa;
+    mesa.appendChild(mesaCliente);
+
+    //mostrar hora
+    const hora = document.createElement('p');
+    hora.textContent = 'Hora: ';
+    hora.classList.add('fw-bold');
+
+    const horaCliente = document.createElement('span');
+    horaCliente.textContent = cliente.hora;
+    hora.appendChild(horaCliente);
+
+    //Mostrar heading:
+    const heading = document.createElement('h3');
+    heading.textContent = 'Pedidos';
+    heading.classList.add('my-4');
+
+    //Producto Pedido:
+    const {pedido} = cliente;
+    pedido.forEach(item=>{
+        const {nombre, cantidad, precio, id} = item;
+
+        const lista = document.createElement('li');
+        lista.classList.add('list-group-item');
+
+        const nombreP = document.createElement('h4');
+        nombreP.textContent = nombre;
+        nombreP.classList.add('text-center', 'my-4');
+
+        const cantidadP = document.createElement('p');
+        cantidadP.textContent = 'Cantidad: ';
+        cantidadP.classList.add('fw-bold');
+
+        const  cantidadValor = document.createElement('span');
+        cantidadValor.textContent = cantidad;
+
+        const precioP = document.createElement('p');
+        precioP.classList.add('fw-bold');
+        precio.textContent = 'precio: ';
+
+        const precioValor = document.createElement('span');
+        precioValor.textContent = `$${precio}`;
+
+
+    lista.appendChild(nombre, cantidadP, cantidadValor, precioP, precioValor);
+
+    })
+
+    resumen.appendChild(heading);
+    resumen.appendChild(mesa);
+    resumen.appendChild(hora);
+
+    contenido.appendChild(resumen);
+}
+
+function limpiarHTML(){
+    const contenido = document.querySelector('#resumen .contenido');
+    while(contenido.firstChild){
+        contenido.removeChild(contenido.firstChild);
+    }
 }
